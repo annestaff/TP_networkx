9dcf570a154ce1134291406813b49003fa4b7169
Anne Staff <github@mlle.io>
2023-09-11 16:44:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import csv
+import os
+
+
+def rwr(network_file, weights_file, outfile):
+    pass
+
+
+if __name__ == "__main__":
+    rwrd = "."
+    network_file = os.path.join(rwrd, "data/PPI_HiUnion_LitBM_APID_gene_names_190123.tsv")
+    weights_file = os.path.join(rwrd, "data/tAML_P3_SNV_polyphen_15390_nodes.tsv")
+    outfile = os.path.join(rwrd, "output/RW_tAML_P3_nx.tsv")
+
+    rwr(network_file, weights_file, outfile)
